8873da871b8ceb3d584c1649af48b24c6220d97f
Alex Rock Ancelet <pierstoval@gmail.com>
2018-10-26 03:28:57 +0200

    Create a minor migration to change a doctrine comment in an sql field

---
+++ src/Migrations/Version20181026012552.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181026012552 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user_subscriptions CHANGE starts_at starts_at DATE NOT NULL COMMENT \'(DC2Type:date_immutable)\', CHANGE ends_at ends_at DATE NOT NULL COMMENT \'(DC2Type:date_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user_subscriptions CHANGE starts_at starts_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE ends_at ends_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+}
