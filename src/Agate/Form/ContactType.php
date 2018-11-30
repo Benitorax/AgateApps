7f0750637aa40c118fbe2af2ad62a7a8da53a163
Alex Rock Ancelet <pierstoval@gmail.com>
2018-11-30 14:18:09 +0100

    Remove deprecation in ContactType

--- src/Agate/Form/ContactType.php
+++ src/Agate/Form/ContactType.php
@@ -53,10 +53,7 @@ class ContactType extends AbstractType
                 'label' => 'contact.form.email',
                 'constraints' => [
                     new Constraints\NotBlank(),
-                    new Constraints\Email([
-                        'checkHost' => 'prod' === $this->kernelEnvironment,
-                        'checkMX' => 'prod' === $this->kernelEnvironment,
-                    ]),
+                    new Constraints\Email(['mode' => Constraints\Email::VALIDATION_MODE_HTML5]),
                 ],
             ])
             ->add('subject', ChoiceType::class, [
