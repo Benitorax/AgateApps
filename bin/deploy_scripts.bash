12333ec0bccc5a5ea5de70a305663bd42621cbdc
Alex Rock Ancelet <pierstoval@gmail.com>
2019-01-20 17:43:42 +0100

    Execute npm scripts *after* php ones on deploy

--- bin/deploy_scripts.bash
+++ bin/deploy_scripts.bash
@@ -8,15 +8,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Project directory
 cd ${DIR}/../
 
-npm install
-
-npm run-script deploy
-
-composer install --no-dev --classmap-authoritative --prefer-dist --no-interaction
-
 php bin/console cache:clear --no-warmup
 php bin/console cache:warmup
 
 php bin/console doctrine:migrations:migrate --no-interaction
 
 php bin/console doctrine:schema:validate || echo 'Doctrine schema not valid, please make sure it is correct.'
+
+npm install
+
+npm run-script deploy
+
+composer install --no-dev --classmap-authoritative --prefer-dist --no-interaction
