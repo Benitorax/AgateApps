8afa2259024bb7a12ccd9caceb0a87039a1dd99a
Alex Rock Ancelet <pierstoval@gmail.com>
2019-02-15 23:14:53 +0100

    Add step 20 details (just miss persistence)

--- src/CorahnRin/Step/Step20Finish.php
+++ src/CorahnRin/Step/Step20Finish.php
@@ -15,15 +15,22 @@ namespace CorahnRin\Step;
 
 use CorahnRin\Exception\CharacterException;
 use CorahnRin\GeneratorTools\SessionToCharacter;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use User\Entity\User;
 
 class Step20Finish extends AbstractStepAction
 {
     private $sessionToCharacter;
+    private $tokenStorage;
 
-    public function __construct(SessionToCharacter $sessionToCharacter)
-    {
+    public function __construct(
+        SessionToCharacter $sessionToCharacter,
+        TokenStorageInterface $tokenStorage
+    ) {
         $this->sessionToCharacter = $sessionToCharacter;
+        $this->tokenStorage = $tokenStorage;
     }
 
     /**
@@ -38,13 +45,29 @@ class Step20Finish extends AbstractStepAction
         try {
             $character = $this->sessionToCharacter->createCharacterFromGeneratorValues($this->getCurrentCharacter());
         } catch (CharacterException $e) {
-            $this->flashMessage('errors.character_not_complete');
-            $this->goToStep(1);
+            $character = null;
         }
 
-        if (\function_exists('dump')) {
-            // Only in dev for now
-            dump($character);
+        if (!$character) {
+            $this->flashMessage('errors.character_not_complete');
+
+            return $this->goToStep(1);
+        }
+
+        $token = $this->tokenStorage->getToken();
+
+        if ($token) {
+            $user = $token->getUser();
+            if ($user instanceof User) {
+                $character->setUser($user);
+            }
+        }
+
+        if ($this->request->isMethod('POST')) {
+            $this->em->persist($character);
+            $this->em->flush();
+
+            return new RedirectResponse($this->router->generate('corahnrin_characters_view', ['id' => $character->getId()]));
         }
 
         return $this->renderCurrentStep([
